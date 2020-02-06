16ae58d79b8ed230d15ce137dd3c84ae22c4cda8
wangyu <14481278@qq.com>
2020-02-06 15:24:52 +0800

    提交了一个新文件

---
+++ src/com/neuedu/test/MyTest1.java
@@ -0,0 +1,12 @@
+package com.neuedu.test;
+
+/**
+ * Created by Neuedu on 2020/2/6.
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        System.out.println(a+b);
+    }
+}
